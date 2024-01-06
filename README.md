8c4ceaae981e2c6ea4ed7a1f93a28eec3244de62
DmitryAlCh <dmi.al.ch@gmail.com>
2024-01-06 11:24:27 +0200

    updated configs

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 * files should be in `~/.config/`
 #### tmux
 * file should be in `~` named `.tmux.conf`
+#### helix
+* file should be in `~./config/helix/config.toml`
+#### wezzterm
+* file should be in `~/.wezzterm.lua`
--- wezzterm/.wezterm.lua
+++ wezzterm/.wezterm.lua
@@ -5,11 +5,22 @@ if  wezterm.config_builder() then
   config = wezterm.config_builder()
 end
 
+config.color_scheme = 'Everforest Light (Gogh)'
 --config.color_scheme = 'Yousai (terminal.sexy)'
 --config.color_scheme = 'Alabaster'
 --config.color_scheme = 'Ashes (light) (terminal.sexy)'
-config.color_scheme = 'Gruvbox Light'
---config.color_scheme = 'Gruvbox Dark'
+--config.color_scheme = 'Gruvbox Light'
+--config.color_scheme = 'BlueDolphin'
+--config.color_scheme = 'Harper'
+--config.color_scheme = 'Github Dark'
+--config.color_scheme = 'Banana Blueberry'
+--config.color_scheme = '3024 (light) (terminal.sexy)'
+--config.color_scheme = '3024 Day (Gogh)'
+--config.color_scheme = 'Gruvbox dark, soft (base16)'
+--config.color_scheme = 'Harmonic16 Dark (base16)'
+--config.color_scheme = 'Harmonic16 Light (base16)'
+--config.color_scheme = 'Hivacruz'
+--config.color_scheme = 'Cobalt2'
 
 config.font = wezterm.font 'Hack'
 config.font_size = 13
